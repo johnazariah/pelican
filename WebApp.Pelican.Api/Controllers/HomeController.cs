004ab705dbdeb8fd8d08198c7d0d1dd1bd15c840
mahesh-krishnan <maheshs_2000@yahoo.com>
2013-08-16 11:49:16 +1000

    Added a Sync button

--- WebApp.Pelican.Api/Controllers/HomeController.cs
+++ WebApp.Pelican.Api/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using MYOB.AccountRight.SDK.Services.GeneralLedger;
+using Pelican.Commands;
+using Pelican.Configuration;
 
 namespace WebApp.Pelican.Api.Controllers
 {
@@ -7,6 +11,31 @@ namespace WebApp.Pelican.Api.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Sync()
+        {
+            var authorizationService = new AuthorizationService();
+            var context = PelicanContext.CreateFromApplicationSettings();
+
+
+            var apiContext = authorizationService.GetAuthorizedContext(context,
+                                                                       context.CompanyFileId);
+
+            var accountService = new AccountService(apiContext.ApiConfiguration,
+                null,
+                apiContext.KeyService);
+
+            accountService.ForeachItem(apiContext.CompanyFile,
+                                                                  apiContext.CompanyFileCredentials,
+                                                                  _ => Console.WriteLine("{0} ({1}) :: {2}-{3:d4} [{4}]",
+                                                                                         _.UID,
+                                                                                         _.RowVersion,
+                                                                                         _.Type,
+                                                                                         _.Number,
+                                                                                         _.Name));
+
+            return View("Index");
         }
     }
 }
